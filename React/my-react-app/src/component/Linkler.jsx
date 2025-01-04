71e90e4127d5d921507deab1718d6859654a9f0b
Öznur TOPÇU <oznurtopcu21@gmail.com>
2025-01-04 16:21:28 +0300

    Linkler olusturuldu.

---
+++ React/my-react-app/src/component/Linkler.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Linkler = () => {
+  return (
+    <nav>
+      <Link to="/">Anasayfa</Link> | 
+      <Link to="/Giris">Üye Girişi</Link> | 
+      <Link to="/Kayit">Üye Kayıt Ol</Link> | 
+      <Link to="/Personel">Personeller</Link>
+    </nav>
+  );
+};
+
+export default Linkler;
