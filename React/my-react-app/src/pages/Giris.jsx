08f488812e7e65caf36a13ca2f8c9f355eda599c
Öznur TOPÇU <oznurtopcu21@gmail.com>
2025-01-04 17:50:50 +0300

    Uye giris eklendi

--- React/my-react-app/src/pages/Giris.jsx
+++ React/my-react-app/src/pages/Giris.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Giris = () => {
+const Giris = ({ uyeler }) => {
+  const [kullaniciAdi, setKullaniciAdi] = useState("");
+  const [sifre, setSifre] = useState("");
+  const [hataMesaji, setHataMesaji] = useState("");
+  const navigate = useNavigate();
+
+  const girisYap = () => {
+    const uye = uyeler.find(
+      (uye) => uye.kullaniciAdi === kullaniciAdi && uye.sifre === sifre
+    );
+
+    if (uye) {
+
+      navigate("/");
+    } else {
+      setHataMesaji("Hatalı kullanıcı adı veya şifre. Lütfen tekrar deneyin.");
+    }
+  };
+
   return (
-    <div>Giris</div>
-  )
-}
+    <>
+      <div>
+        <h1>Üye Girişi</h1>
+        {hataMesaji && <p style={{ color: "red" }}>{hataMesaji}</p>}
+        <input
+          type="text"
+          placeholder="Kullanıcı Adı"
+          value={kullaniciAdi}
+          onChange={(e) => setKullaniciAdi(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Şifre"
+          value={sifre}
+          onChange={(e) => setSifre(e.target.value)}
+        />
+        <button onClick={girisYap}>Giriş Yap</button>
+      </div>
+    </>
+  );
+};
 
-export default Giris
+export default Giris;
