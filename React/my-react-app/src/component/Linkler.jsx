4ea29070eca0591d123783d2dd92ceee52fdb2b3
Öznur TOPÇU <oznurtopcu21@gmail.com>
2025-01-05 09:38:35 +0300

    link düzeltildi

--- React/my-react-app/src/component/Linkler.jsx
+++ React/my-react-app/src/component/Linkler.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Linkler = () => {
   return (
     <nav>
-      <Link to="/">Anasayfa</Link> | 
-      <Link to="/Giris">Üye Girişi</Link> | 
-      <Link to="/Kayit">Üye Kayıt Ol</Link> | 
+      <Link to="/">Anasayfa</Link> 
+      <Link to="/Giris">Üye Girişi</Link>  
+      <Link to="/Kayit">Üye Kayıt Ol</Link>  
       <Link to="/Personel">Personeller</Link>
     </nav>
   );
